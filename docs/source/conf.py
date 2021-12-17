1647e7cc06cc5bce306afc5d4e2f66862f3be701
fdosani <faisal.dosani@protonmail.com>
2021-12-17 19:26:16 -0400

    Bump 2021.12.4 to 2021.12.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = "2021, Akshay Gupta"
 author = "Akshay Gupta"
 
 # The short X.Y version
-version = "2021.12.4"
+version = "2021.12.5"
 # The full version, including alpha/beta/rc tags
 release = ""
 
