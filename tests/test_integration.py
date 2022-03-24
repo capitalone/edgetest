37e6302b09df4c3fad3a5cdf621ac6460f5819c8
Faisal <faisal.dosani@capitalone.com>
2022-03-24 15:59:36 -0300

    Extras upgrade bug fix (#15)
    * Fixes #14, avoid conflict with upgrade[extras]
    
    * updating integration testing for extras
    
    * bumping version to 2022.3.1
    
    * skipping some windows asserts

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,5 +1,6 @@
 """Create a fake package and test."""
-
+import configparser
+import sys
 from pathlib import Path
 
 import pytest
@@ -31,6 +32,36 @@ extras =
     tests
 """
 
+
+SETUP_CFG_DASK = """
+[metadata]
+name = toy_package
+version = 0.1.0
+description = Fake description
+python_requires =
+    >=3.7.0
+
+[options]
+zip_safe = False
+include_package_data = True
+packages = find:
+install_requires =
+    click<=8.0.0,>=7.1
+    dask[dataframe]<=2022.1.0
+
+[options.extras_require]
+tests =
+    pytest
+
+[edgetest.envs.core]
+extras =
+    tests
+upgrade =
+    click
+    dask[dataframe]
+"""
+
+
 SETUP_PY = """
 from setuptools import setup
 
@@ -49,7 +80,10 @@ def test_func():
     main()
 """
 
+PY_VER = f"python{sys.version_info.major}.{sys.version_info.minor}"
 
+
+@pytest.mark.skipif(sys.platform == "win32", reason="does not run on windows")
 @pytest.mark.integration
 def test_toy_package():
     """Test using edgetest with a toy package."""
@@ -75,3 +109,54 @@ def test_toy_package():
         assert result.exit_code == 0
         assert Path(loc, ".edgetest").is_dir()
         assert Path(loc, ".edgetest", "pandas").is_dir()
+
+        if not sys.platform == "win32":
+            assert Path(
+                loc, ".edgetest", "pandas", "lib", PY_VER, "site-packages", "pandas"
+            ).is_dir()
+
+
+@pytest.mark.integration
+def test_toy_package_dask():
+    """Test using edgetest with a toy package."""
+    runner = CliRunner()
+
+    with runner.isolated_filesystem() as loc:
+        with open("setup.cfg", "w") as outfile:
+            outfile.write(SETUP_CFG_DASK)
+        with open("setup.py", "w") as outfile:
+            outfile.write(SETUP_PY)
+        # Make a directory for the module
+        Path(loc, "toy_package").mkdir()
+        with open(Path(loc, "toy_package", "__init__.py"), "w") as outfile:
+            outfile.write(MODULE_CODE)
+        # Make a directory for the tests
+        Path(loc, "tests").mkdir()
+        with open(Path(loc, "tests", "test_main.py"), "w") as outfile:
+            outfile.write(TEST_CODE)
+
+        # Run the CLI
+        result = runner.invoke(cli, ["--config=setup.cfg", "--export"])
+
+        assert result.exit_code == 0
+        assert Path(loc, ".edgetest").is_dir()
+        assert Path(loc, ".edgetest", "core").is_dir()
+
+        if not sys.platform == "win32":
+            assert Path(
+                loc, ".edgetest", "core", "lib", PY_VER, "site-packages", "dask"
+            ).is_dir()
+            assert Path(
+                loc, ".edgetest", "core", "lib", PY_VER, "site-packages", "pandas"
+            ).is_dir()
+            assert Path(
+                loc, ".edgetest", "core", "lib", PY_VER, "site-packages", "click"
+            ).is_dir()
+
+        config = configparser.ConfigParser()
+        config.read("setup.cfg")
+
+        assert "click" in config["options"]["install_requires"]
+        assert "dask[dataframe]" in config["options"]["install_requires"]
+        assert config["edgetest.envs.core"]["extras"] == "\ntests"
+        assert config["edgetest.envs.core"]["upgrade"] == "\nclick\ndask[dataframe]"
