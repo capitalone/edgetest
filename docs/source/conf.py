3c7240f2468505721b6dae2059fb93adc6f5f3e0
Faisal <faisal.dosani@pm.me>
2025-10-02 10:39:11 -0400

    chore: update version to 2025.10.0 and adjust tomlkit dependency range (#101)
    * chore: update version to 2025.10.0 and adjust tomlkit dependency range
    
    * chore: update tomlkit version in requirements.txt and add dev-requirements.txt

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = "2021, Akshay Gupta"
 author = "Akshay Gupta"
 
 # The short X.Y version
-version = "2025.8.0"
+version = "2025.10.0"
 # The full version, including alpha/beta/rc tags
 release = ""
 
